af2cee65a35434d05830be39569e889f61df87f2
danielzuhad <danielzuhad@gmail.com>
2025-11-05 17:02:22 +0700

    done form create item

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,5 +1,5 @@
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const HomePage = async () => {
   const session = await getServerSession(authOptions);
